114d9a21843d66eec1df4dc9e626b73e967cf680
Ayoub <elhoudaiguiayoub2@gmail.com>
2026-02-13 20:44:45 -0800

    Fix vite env types

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,21 +1,6 @@
 /// <reference types="vite/client" />
 
-declare module "*.png" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.jpg" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.jpeg" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.svg" {
-  const src: string;
-  export default src;
-}
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.svg";
